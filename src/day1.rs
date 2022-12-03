b6af8cfa316023acc5cba32a26ec87c37581653b
Jacob Gold <jacob@graphite.dev>
2022-12-03 16:42:57 -0500

    day1: part2

--- src/day1.rs
+++ src/day1.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::vec::Vec;
 
 pub fn get_max_calories() -> i32 {
     return fs::read_to_string("input/1.txt")
@@ -13,4 +14,20 @@ pub fn get_max_calories() -> i32 {
         })
         .max()
         .unwrap_or_default();
+}
+
+pub fn get_top3_calories() -> i32 {
+    let mut elves = fs::read_to_string("input/1.txt")
+        .expect("Failed to read file")
+        .split('\n')
+        .collect::<Vec<&str>>()
+        .split(|&line| line == "")
+        .map(|l| {
+            l.iter()
+                .map(|&s| str::parse::<i32>(s).expect("Failed to parse string"))
+                .sum()
+        })
+        .collect::<Vec<i32>>();
+    elves.sort_by(|a, b| b.cmp(a));
+    return elves.iter().take(3).sum();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod day1;
 
 fn main() {
-    println!("{}", day1::get_max_calories())
+    println!("{}", day1::get_max_calories());
+    println!("{}", day1::get_top3_calories());
 }
