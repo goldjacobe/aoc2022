c619dccb0561f27a502e128daf9679158c249cf7
Jacob Gold <jacob@graphite.dev>
2022-12-03 21:00:56 -0500

    day2: part1

---
+++ src/day2.rs
@@ -0,0 +1,33 @@
+use std::fs;
+
+pub fn get_total_score() -> i32 {
+    return fs::read_to_string("input/2.txt")
+        .expect("Failed to read file")
+        .split('\n')
+        .filter(|line| line.len() > 2)
+        .map(|line| {
+            let abc = map_left_char(line.chars().nth(0).expect("should have a char"));
+            let xyz = map_right_char(line.chars().nth(2).expect("should have a char"));
+            let result = (xyz - abc + 4) % 3;
+            return result * 3 + xyz + 1;
+        })
+        .sum();
+}
+
+fn map_left_char(c: char) -> i32 {
+    match c {
+        'A' => 0,
+        'B' => 1,
+        'C' => 2,
+        _ => panic!("oopsie, [{}]", c),
+    }
+}
+
+fn map_right_char(c: char) -> i32 {
+    match c {
+        'X' => 0,
+        'Y' => 1,
+        'Z' => 2,
+        _ => panic!("oopsie, [{}]", c),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod day1;
+mod day2;
 
 fn main() {
     println!("{}", day1::get_max_calories());
     println!("{}", day1::get_top3_calories());
+    println!("{}", day2::get_total_score());
 }
