1f6d62dbe4115b77284d872e230559e30697075f
Jacob Gold <jacob@graphite.dev>
2022-12-04 20:35:08 -0500

    refactor: day4

--- src/day4.rs
+++ src/day4.rs
@@ -1,47 +1,35 @@
 use std::fs;
 
-pub fn get_num_pairs_with_full_contain() -> usize {
+pub fn get_num_pairs_with_full_contain() -> i32 {
     return fs::read_to_string("input/4.txt")
         .expect("Failed to read file")
         .split('\n')
-        // .filter(|&line| line.len() > 0)
         .map(|s| {
-            let v = s
-                .replace('-', ",")
+            s.replace('-', ",")
                 .split(',')
                 .map(|n| n.parse::<i32>().unwrap())
-                .collect::<Vec<i32>>();
-
-            if v.get(0) <= v.get(2) && v.get(1) >= v.get(3)
-                || v.get(0) >= v.get(2) && v.get(1) <= v.get(3)
-            {
-                return 1;
-            } else {
-                return 0;
-            };
+                .collect::<Vec<i32>>()
+        })
+        .map(|v| {
+            (v.get(0) <= v.get(2) && v.get(1) >= v.get(3)
+                || v.get(0) >= v.get(2) && v.get(1) <= v.get(3)) as i32
         })
         .sum();
 }
 
-pub fn get_num_pairs_with_overlap() -> usize {
+pub fn get_num_pairs_with_overlap() -> i32 {
     return fs::read_to_string("input/4.txt")
         .expect("Failed to read file")
         .split('\n')
-        // .filter(|&line| line.len() > 0)
         .map(|s| {
-            let v = s
-                .replace('-', ",")
+            s.replace('-', ",")
                 .split(',')
                 .map(|n| n.parse::<i32>().unwrap())
-                .collect::<Vec<i32>>();
-
-            if v.get(0) <= v.get(2) && v.get(1) >= v.get(2)
-                || v.get(0) >= v.get(2) && v.get(0) <= v.get(3)
-            {
-                return 1;
-            } else {
-                return 0;
-            };
+                .collect::<Vec<i32>>()
+        })
+        .map(|v| {
+            (v.get(0) <= v.get(2) && v.get(1) >= v.get(2)
+                || v.get(0) >= v.get(2) && v.get(0) <= v.get(3)) as i32
         })
         .sum();
 }
